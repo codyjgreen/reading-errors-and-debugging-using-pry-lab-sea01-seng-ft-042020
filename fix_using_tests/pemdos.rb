3e3fcc079ef6aaf71cda7e71e1106acca4759ff2
Cody Green <cody.green@flatironschool.com>
2020-05-04 19:27:43 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,16 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
+    
     10 * "s" + string
+    binding.pry
   else
     string
   end
 end
+
+snake_it_up(cherry)
+
+snake_it_up(scherry)
